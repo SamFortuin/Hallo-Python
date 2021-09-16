7cb146da98d9981caf84765d5db993aa63aebd6d
Sam Fortuin <99059050@mydavinci.nl>
2021-09-16 11:11:18 +0200

    commit4

---
+++ hello.py
@@ -0,0 +1,3 @@
+#99059050, Sam Fortuin
+#Pints the string with enters using \n. decreases readability, increases smoothness(?)
+print("Hello wereld van…\n\npppp    y     y   ttttttt   h   h    oooo   nnnn    \np   p    y   y       t      h   h   o    o  n   n   \np   p     y y        t      h   h   o    o  n   n   \npppp       y         t      hhhhh   o    o  n   n   \np          y         t      h   h   o    o  n   n   \np          y         t      h   h   o    o  n   n   \np          y         t      h   h    oooo   n   n   \n")
